9a7f5f73c5a9e615669e295ceb8395e988f9df67
manduong <minhmanuzu@gmail.com>
2023-04-14 10:05:11 +0700

    giao diện

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,20 +10,23 @@ namespace ConsoleApp1
     internal class Program
     {
         static DANHSACH danhsach;
-        
+
 
         static void Menu()
         {
             try
             {
-                Console.WriteLine("Nhap tu 1-6 de thuc hien cac chuc nang sau: ");
-                Console.WriteLine("1. Chuyen tien");
-                Console.WriteLine("2. Nap tien/rut vao app");
-                Console.WriteLine("4. Thanh Toan hoa don");
-                Console.WriteLine("5. Lich su giao dich");
+
+                Console.WriteLine("\x1b[94m-----------------------------------------------");
+                Console.WriteLine("| Nhap tu 1-6 de thuc hien cac chuc nang sau: |");
+                Console.WriteLine("| 1. Chuyen tien                              |");
+                Console.WriteLine("| 2. Nap tien/rut vao app                     |");
+                Console.WriteLine("| 3. Thanh Toan hoa don                       |");
+                Console.WriteLine("| 4. Lich su giao dich                        |");
+                Console.WriteLine("-----------------------------------------------\x1b[0m");
                 int menu = 0;
-                menu= Convert.ToInt32(Console.ReadLine());
-                switch(menu)
+                menu = Convert.ToInt32(Console.ReadLine());
+                switch (menu)
                 {
                     case 1:
                         {
@@ -34,19 +38,19 @@ namespace ConsoleApp1
                             danhsach.NhapNaprut();
                             break;
                         }
-                    case 5:
+                    case 4:
                         {
                             danhsach.lichsugd();
                             break;
-                        } 
-                   
-                    
+                        }
+
                     default:
                         Console.WriteLine("Yeu cau nhap chuc nang menu  tu 1-5 ");
                         Menu();
                         break;
                 }//end switch
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
                 Menu();
             }
@@ -56,11 +60,11 @@ namespace ConsoleApp1
         {
             danhsach = new DANHSACH();
             char c = 'y';
-            while (c== 'y')
+            while (c == 'y')
             {
                 Menu();
                 Console.WriteLine("Nhap ky tu 'y' de tro ve menu");
-                c= Convert.ToChar(Console.ReadLine().ToLower());
+                c = Convert.ToChar(Console.ReadLine().ToLower());
             }
         }
     }
